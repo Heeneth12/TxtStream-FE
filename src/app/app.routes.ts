ddcfc674f63b5197250f3a94aab380eb42070528
Heeneth-sai <heeneth.sai@dragarwal.com>
2024-08-23 12:09:26 +0530

    created base template

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { LandingPageComponent } from './views/landing-page/landing-page.component';
+import { LoginPageComponent } from './views/login-page/login-page.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: LandingPageComponent },
+  { path: 'login', component: LoginPageComponent },
+];
